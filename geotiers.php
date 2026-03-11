8d567c8d7e4e2c1632bf73c0588a9c32414d0cbd
AGHILES667 <ghilesouaras2@gmail.com>
2026-03-11 10:38:04 +0100

    Translations

--- geotiers.php
+++ geotiers.php
@@ -102,7 +102,7 @@ $multiTypeColor = getDolGlobalString('GEOTIERS_COLOR_MULTI_TYPE', '#eca76a');
             
             <h1><?php echo $langs->trans("GeoTiersMapTitle"); ?></h1>
 
-            <button id="btnFullscreen" class="flgeotiers-fullscreen-btn" title="Plein écran">
+            <button id="btnFullscreen" class="flgeotiers-fullscreen-btn" title="<?php echo $langs->trans("FullScreen"); ?>">
                 <svg id="iconExpand" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="15 3 21 3 21 9"/><polyline points="9 21 3 21 3 15"/><line x1="21" y1="3" x2="14" y2="10"/><line x1="3" y1="21" x2="10" y2="14"/></svg>
                 <svg id="iconCompress" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" style="display:none"><polyline points="4 14 10 14 10 20"/><polyline points="20 10 14 10 14 4"/><line x1="10" y1="14" x2="3" y2="21"/><line x1="21" y1="3" x2="14" y2="10"/></svg>
             </button>
